865a922127dc2d5d9286b0434061022819b5ac02
phamcongthanhvn2k6 <phamcongt56@gmail.com>
2024-12-18 08:09:26 +0700

    Update baitapluyentapvonglap12.cpp

--- baitapluyentapvonglap12.cpp
+++ baitapluyentapvonglap12.cpp
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int n, t1 = 0, t2 = 1, nextTerm;
+    int n, t1 = 0, t2 = 1, bientam;
 
-    printf("Nhap v‡o so luong so Fibonacci can in: ");
+    printf("Nhap v√†o so luong so Fibonacci can in: ");
     scanf("%d", &n);
 
     printf("Day so Fibonacci: ");
 
     for (int i = 1; i <= n; ++i) {
         printf("%d\n ", t1);
-        nextTerm = t1 + t2;
+        bientam = t1 + t2;
         t1 = t2;
-        t2 = nextTerm;
+        t2 = bientam;
     }
     
     printf("\n");
     return 0;
 }
-
